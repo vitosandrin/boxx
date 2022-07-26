2106be2688ac1b2e813fde293be407f2cf2424ea
Vito <sandrinvito10@gmail.com>
2022-07-26 18:51:10 -0300

    feat: aboutSection finsih 2/3

--- src/pages/Home/components/AboutSection/index.jsx
+++ src/pages/Home/components/AboutSection/index.jsx
@@ -16,32 +16,54 @@ const AboutSection = () => {
                 <div className={styles.box_aboutinfo}>
                     <img className={styles.img_aboutinfo} src={document} alt="" />
                     <h3 className={styles.title_aboutinfo}>Briefing</h3>
-                    <p className={styles.description_aboutinfo}>Briefing</p>
+                    <p className={styles.description_aboutinfo}>
+                        Coletamos dados e informações sobre seu projeto
+                        e suas particularidades com um questionário
+                        padrão operacional.
+                    </p>
                 </div>
                 <div className={styles.box_aboutinfo}>
                     <img className={styles.img_aboutinfo} src={talk} alt="" />
-                    <h3 className={styles.title_aboutinfo}>Talk</h3>
-                    <p className={styles.description_aboutinfo}>Talk</p>
+                    <h3 className={styles.title_aboutinfo}>Call</h3>
+                    <p className={styles.description_aboutinfo}>
+                        Agendamos uma reunião para apresentar nossa
+                        proposta após ter feito uma cotação exclusiva.
+                    </p>
                 </div>
                 <div className={styles.box_aboutinfo}>
                     <img className={styles.img_aboutinfo} src={selected} alt="" />
-                    <h3 className={styles.title_aboutinfo}>Selected</h3>
-                    <p className={styles.description_aboutinfo}>Selected</p>
+                    <h3 className={styles.title_aboutinfo}>Aprovação</h3>
+                    <p className={styles.description_aboutinfo}>
+                        Apresentamos e procuramos as melhores alternativas
+                        para atender as solicitações do usuário.
+                    </p>
                 </div>
                 <div className={styles.box_aboutinfo}>
                     <img className={styles.img_aboutinfo} src={contract} alt="" />
-                    <h3 className={styles.title_aboutinfo}>Contract</h3>
-                    <p className={styles.description_aboutinfo}>Contract</p>
+                    <h3 className={styles.title_aboutinfo}>Contrato</h3>
+                    <p className={styles.description_aboutinfo}>
+                        Estabelecemos contrato de prestação de serviço
+                        para resguardar os interesses do contratante
+                        e contratado.
+                    </p>
                 </div>
                 <div className={styles.box_aboutinfo}>
                     <img className={styles.img_aboutinfo} src={sprint} alt="" />
-                    <h3 className={styles.title_aboutinfo}>Sprint</h3>
-                    <p className={styles.description_aboutinfo}>Sprint</p>
+                    <h3 className={styles.title_aboutinfo}>Sprints</h3>
+                    <p className={styles.description_aboutinfo}>
+                        Sprints, relatórios e reuniões definidas
+                        em contrato conforme necessidade e prazo
+                        do projeto.
+                    </p>
                 </div>
                 <div className={styles.box_aboutinfo}>
                     <img className={styles.img_aboutinfo} src={finished} alt="" />
-                    <h3 className={styles.title_aboutinfo}>Finished</h3>
-                    <p className={styles.description_aboutinfo}>Finished</p>
+                    <h3 className={styles.title_aboutinfo}>Entrega</h3>
+                    <p className={styles.description_aboutinfo}>
+                        Prezamos por uma entrega profissional, respeitando
+                        prazos e responsabilidades para maior satisfação
+                        dos clientes.
+                    </p>
                 </div>
             </div>
         </section>
