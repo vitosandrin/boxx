6de1307e043a736a0a0e35ef07abd5122c665e04
Vito <sandrinvito10@gmail.com>
2022-07-26 01:20:53 -0300

    fix: formEmail

--- src/components/FormEmail/index.jsx
+++ src/components/FormEmail/index.jsx
@@ -20,7 +20,7 @@ const FormEmail = () => {
 
     return (
         <div className={styles.container_formemail}>
-            <h2 className={styles.title_formemail}>Contate-nos e tire todas suas dúvidas!</h2>
+            <h2 className={styles.title_formemail}>Contate-nos e faça um orçamento!</h2>
             <form className={styles.form_control} onSubmit={sendEmail}>
                 <div className={styles.section_formemail}>
                     <input className={styles.input_formemail} type="text" required placeholder="Nome" name="name" />
