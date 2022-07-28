108481d6345d24fd43f4f7a8196b9549ed4c2d9e
Vito <sandrinvito10@gmail.com>
2022-07-28 15:36:34 -0300

    fix docs

--- src/pages/Home/components/ServiceSection/index.jsx
+++ src/pages/Home/components/ServiceSection/index.jsx
@@ -12,44 +12,44 @@ const ServiceSection = () => {
         <section className={styles.container_service}>
             <h2 className={styles.title_service}>Por que ser parceiro .boxx?</h2>
             <div className={styles.box_service}>
-                <p className={styles.description_service}>
+                <div className={styles.description_service}>
                     <h3 className={styles.title_info}>Objetividade</h3>
                     Na .boxx, somos adeptos ao "clean code" e metodologias
                     ágeis de trabalho.
-                </p>
+                </div>
                 <img className={styles.img_boxservice} src={icone5} alt="" />
             </div>
 
             <div className={styles.box_service}>
                 <img className={styles.img_boxservice} src={icone1} alt="" />
-                <p className={styles.description_service}>
+                <div className={styles.description_service}>
                     <h3 className={styles.title_info}>Atualização</h3>
                     Encontramos a lógica ideal para criar e progredir
                     um código-fonte eficaz e manutenível.
-                </p>
+                </div>
             </div>
 
             <div className={styles.box_service}>
-                <p className={styles.description_service}>
+                <div className={styles.description_service}>
                     <h3 className={styles.title_info}>Conexão</h3>
                     Integrações sofisticadas com excelente acessibilidade
                     do usuário.
-                </p>
+                </div>
                 <img className={styles.img_boxservice} src={icone7} alt="" />
             </div>
             <div className={styles.box_service}>
                 <img className={styles.img_boxservice} src={icone4} alt="" />
-                <p className={styles.description_service}>
+                <div className={styles.description_service}>
                     <h3 className={styles.title_info}>Exclusividade</h3>
                     Projetos desenvolvidos exclusivamente, do software ao design.
-                </p>
+                </div>
             </div>
             <div className={styles.box_service}>
-                <p className={styles.description_service}>
+                <div className={styles.description_service}>
                     <h3 className={styles.title_info}>Competitividade</h3>
                     Insights em todo cronograma de produção visando
                     destacar diferenciais competitivos.
-                </p>
+                </div>
                 <img className={styles.img_boxservice} src={icone6} alt="" />
             </div>
             <img className={styles.img_service} src={icone8} alt="" />
