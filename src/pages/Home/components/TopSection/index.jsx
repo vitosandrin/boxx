f32bd4fbafb904e0ebe1db7b3ab07e79f54a3239
Vito <sandrinvito10@gmail.com>
2022-07-28 20:23:22 -0300

    feat: lottie top

--- src/pages/Home/components/TopSection/index.jsx
+++ src/pages/Home/components/TopSection/index.jsx
@@ -1,4 +1,6 @@
 import styles from './styles.module.css';
+import rocket from '../../../../assets/json/rocket.json'
+import { Loader } from '../../../../components'
 
 const TopSection = () => {
     return (
@@ -7,6 +9,7 @@ const TopSection = () => {
                 <h6 className={styles.title_outline}>Desenvolvimento</h6>
                 <p className={styles.description_outline}>Tecnologias e ferramentas de última geração</p>
             </div>
+            <Loader width={175} height={175} data={rocket}/>
             <div className={styles.box_outline}>
                 <h6 className={styles.title_outline}>Design</h6>
                 <p className={styles.description_outline}>UI/UIX de alta conversão</p>
